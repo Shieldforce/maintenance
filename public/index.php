c081d80b54127572d05f74ea2fb1058c99bab0b9
Alexandre Ferreira <shieldforce2@gmail.com>
2020-01-28 09:33:46 -0300

    site em construção

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema em Manutenção</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+
+<body>
+    <div class="div-main">
+        <h1>Sitema em construção</h1>
+    </div>
+</body>
+
+</html>
