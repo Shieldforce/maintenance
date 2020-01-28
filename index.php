401e1e37d83466d1ab88d98d4eed2df4a6f250bf
Alexandre Ferreira <shieldforce2@gmail.com>
2020-01-28 10:33:09 -0300

    finish

--- index.php
+++ index.php
@@ -4,14 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistema em Manutenção</title>
     <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
 
-<body>
+<body style="background-image: url('fundo.jpg');">
     <div class="div-main">
-        <h1>Sistema em construção</h1>
+        <div class="div-title">
+            <h1>Sistema em construção</h1>
+        </div>
+        <div class="div-paragraph">
+            <p>Contato: (21)97018-5540 ou shieldforce2@gmail.com</p>
+        </div>
     </div>
 </body>
 
